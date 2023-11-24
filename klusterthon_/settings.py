6935d9e8b09b24fe60620c157174fd9225da65f9
Abdul12majid <yisaabdulmajid@gmail.com>
2023-11-24 08:47:01 +0100

    deploy_

--- klusterthon_/settings.py
+++ klusterthon_/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
